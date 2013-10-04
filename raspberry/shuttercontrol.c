c6f2ac4ae1ce184e104d5d2f9c0845ecf7757a4a
Penguineer <tux@netz39.de>
2013-10-04 23:30:29 +0200

    Shutter control functions

--- raspberry/shuttercontrol.c
+++ raspberry/shuttercontrol.c
@@ -7,7 +7,7 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-#define I2C_ADDR_CONTROLLER 0x20
+#define I2C_ADDR_CONTROLLER 0x21
 #define I2C_ADDR_MANUAL     0x22
 
 ///// I2C stuff /////
@@ -30,7 +30,7 @@ struct I2C_descriptors {
   * @param addr The target address.
   * @return File Descriptor for the I2C channel
   */
-int I2C_setup_fd(int addr) {
+int I2C_setup_fd(const int addr) {
   const int fd = wiringPiI2CSetup(addr);
   if (!fd) {
     printf("Error %d on I2C initialization!", errno);
@@ -49,7 +49,7 @@ void I2C_init(void) {
 
 #define I2C_ERR_INVALIDARGUMENT -2
 
-int I2C_command(int fd, char command, char data) {
+int I2C_command(const int fd, const char command, const char data) {
   // check parameter range
   if ((command < 0) || (command > 0x0f))
     return I2C_ERR_INVALIDARGUMENT;
@@ -74,11 +74,11 @@ int I2C_command(int fd, char command, char data) {
 
 ///// I3C stuff /////
 
-void I3C_reset() {
+void I3C_reset_manual() {
   I2C_command(I2C_FD_MANUAL, 0x4, 0x0);
 }
 
-///// Switch states /////
+///// Manual Controll unit /////
 
 #define SWITCH_UP      1
 #define SWITCH_NEUTRAL 3
@@ -87,7 +87,13 @@ void I3C_reset() {
 #define SWITCH_ERR             -1
 #define SWITCH_ERR_OUTOFBOUNDS -2
 
-char read_switch_state(int idx) {
+/**
+  * Read the state of the specified switch.
+  * @return 
+  *	Switch state according to SWITCH_XXX, SWITCH_ERR if an error
+  *	occurred.
+  */
+char read_switch_state(const char idx) {
   // check parameter range
   if ((idx < 1) || (idx > 3))
     return SWITCH_ERR_OUTOFBOUNDS;
@@ -99,19 +105,67 @@ char read_switch_state(int idx) {
   return state;  
 }
 
-void beep(char pattern) {
+/**
+  * Beep in the provided pattern.
+  * @param The beep pattern. Only the last 4 Bits are evaluated!
+  */
+void beep(const char pattern) {
   I2C_command(I2C_FD_MANUAL, 0x1, pattern&0xf);
 }
-
 
 #define LED_PATTERN_OFF  0x00
 #define LED_PATTERN_SLOW 0x01
 #define LED_PATTERN_FAST 0x02
 #define LED_PATTERN_ON   0x03
 
+/**
+  * Set manual mode LED to the specified blink pattern.
+  * @param pattern The blink pattern; one of LED_PATTERN_XXX.
+  */
 void set_manual_mode_led(const char pattern) {
   I2C_command(I2C_FD_MANUAL, 0x2, pattern);
 }
+
+///// Shutter Control unit /////
+
+#define SHUTTER_ERR             -1
+#define SHUTTER_ERR_OUTOFBOUNDS -2
+
+#define SHUTTER_OFF  0
+#define SHUTTER_UP   1
+#define SHUTTER_DOWN 2
+
+char set_shutter_state(const char idx, const char state) {
+  // check parameter range
+  if ((idx < 1) || (idx > 3))
+    return SHUTTER_ERR_OUTOFBOUNDS;
+  if ((state < 0) || (state > 2))
+    return SHUTTER_ERR_OUTOFBOUNDS;  
+
+  // determine the command
+  char command = 0x0;
+  switch (state) {
+    case SHUTTER_OFF:  command = 0x1; break;
+    case SHUTTER_UP:   command = 0x2; break;
+    case SHUTTER_DOWN: command = 0x3; break;
+    default: {
+      printf("set_shutter_state: Unknown shutter state: %d.");
+      printf("This cannot happen!");
+      exit(-1);
+    }
+  }
+
+  // send the command    
+  I2C_command(I2C_FD_CONTROLLER, command, idx);
+
+  // return OK
+  return 0;
+}
+
+void stop_all_shutters() {
+  I2C_command(I2C_FD_CONTROLLER, 0x0, 0x0);
+}
+
 
 int main(int argc, char *argv[]) {
   I2C_init();
@@ -122,18 +176,16 @@ int main(int argc, char *argv[]) {
   while(run) {
     char sw = read_switch_state(0x1);
     printf("Switch status: %d\n", sw);
-    I3C_reset();
+    I3C_reset_manual();
     
     switch (sw) {
-      case SWITCH_NEUTRAL: set_manual_mode_led(LED_PATTERN_ON); break;
-      case SWITCH_UP: set_manual_mode_led(LED_PATTERN_SLOW); break;
-      case SWITCH_DOWN: set_manual_mode_led(LED_PATTERN_FAST); break;
+      case SWITCH_NEUTRAL: set_shutter_state(0x1, SHUTTER_OFF); break;
+      case SWITCH_UP: set_shutter_state(0x1, SHUTTER_UP); break;
+      case SWITCH_DOWN: set_shutter_state(0x1, SHUTTER_DOWN); break;
     }
 
     if (sleep(1)) 
       break;
-      
-//    wiringPiI2CWrite(I2C_FD_MANUEL, 0x20+(sw&0x0f));
   }
   
   return 0;
