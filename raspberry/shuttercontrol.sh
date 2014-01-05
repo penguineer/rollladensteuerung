ad1ce7e5d2cef9733c65e4dd083239823a705414
Penguineer <tux@netz39.de>
2014-01-05 13:02:33 +0100

    Watchdog ein/ausschalten über Pin 4 (GPIO 23)

--- raspberry/shuttercontrol.sh
+++ raspberry/shuttercontrol.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+gpio mode 4 out
+gpio write 4 1
+
+sleep 2
+
 gpio load i2c
 ./shuttercontrol
+
+gpio write 4 0
+gpio mode 4 tri
---
+++ raspberry/switch-off.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gpio write 4 0
+gpio mode 4 tri
