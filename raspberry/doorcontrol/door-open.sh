4f43d3c69f74c8079a5df27b0daff1f7f1faa4ff
Tux <tux@netz39.de>
2015-12-05 13:49:03 +0100

    door-open: Error count and reset on I2C problems
    Count I3C transmission errors and reset the system if there are too many
    errors in a row.
    
    Also logs actions to the shuttercontrol log.

--- raspberry/doorcontrol/door-open.sh
+++ raspberry/doorcontrol/door-open.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 caller=$(ps -ocommand= -p $PPID)
-logger -t shuttercontrol Opening door from shell script via $caller.
+logger -t shuttercontrol "Opening door from shell script via $caller."
 
 gpio load i2c
 
 ret=""
+errcount=0
 while [[ "$ret" != "0x01" ]]; do
 	ret=$(/usr/sbin/i2cget -y 1 0x23 0x90)
 	echo $ret
+	errcount=$(($errcount+1))
+	if [[ $errcount -eq 10 ]]; then
+		logger -t shuttercontrol "I2C seems to be stuck, restarting the hardware."
+		sleep 5
+		~/unstuck.sh
+		sleep 5
+	fi
 done
 
