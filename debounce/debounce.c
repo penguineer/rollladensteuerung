742755239844286fa5ea786cd07ab2286f5cb6f9
Tux <tux@netz39.de>
2016-04-30 14:28:46 +0200

    Debounce library code quality
    Add fixes regarding https://github.com/netz39/rollladensteuerung/pull/1#issuecomment-214857090

--- debounce/debounce.c
+++ debounce/debounce.c
@@ -1,51 +1,72 @@
-/*
- * De-bouncing functions accoding to
- * http://hackaday.com/2015/12/10/embed-with-elliot-debounce-your-noisy-buttons-part-ii/
+/**
+ * @file debounce.c
+ * @author Stefan Haun (tux@netz39.de)
  *
- * Author: tux@netz39.de
+ * @brief Pattern-based de-bouncing functions
+ *
+ * @see http://hackaday.com/2015/12/10/embed-with-elliot-debounce-your-noisy-buttons-part-ii/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "debounce.h"
 
-#define DEBOUNCE_MASK	0b11000111
+/*
+ * If you change this mask, the pattern masks in the below functions must
+ * be changed as well!
+ */
+#define DEBOUNCE_MASK 0b11000111
 
-void debounce_init_button(uint8_t *history) {
-  *history = 0xff;
-}
-
-void debounce_update_button(uint8_t *history,
-		   uint8_t pinport, uint8_t pinpin)
+void debounce_init_button ( uint8_t *history )
 {
-  *history = *history << 1;
-
-  // read the button and store to history
-  *history |= ((pinport & (1 << pinpin)) == 0);
+    *history = 0b11111111;
 }
 
-uint8_t debounce_is_button_pressed(uint8_t *history)
+void debounce_update_button ( uint8_t *history,
+                              uint8_t pinport, uint8_t pinpin )
 {
-  uint8_t pressed = 0;
-  if ((*history & DEBOUNCE_MASK) == 0b00000111) {
-    pressed = 1;
-    *history = 0xff;
-  }
-  return pressed;
+    *history = *history << 1;
+
+    // read the button and store to history
+    *history |= ( ( pinport & ( 1 << pinpin ) ) == 0 );
 }
 
-uint8_t debounce_is_button_released(uint8_t *history)
+bool debounce_is_button_pressed ( uint8_t *history )
 {
-  uint8_t released = 0;
-  if ((*history & DEBOUNCE_MASK) == 0b11000000) {
-    released = 1;
-    *history = 0x00;
-  }
-  return released;
+    bool pressed = false;
+    if ( ( *history & DEBOUNCE_MASK ) == 0b00000111 ) {
+        pressed = true;
+        *history = 0b11111111;
+    }
+    return pressed;
 }
 
-uint8_t debounce_is_button_down(uint8_t *history){
-  return *history == 0xff;
+bool debounce_is_button_released ( uint8_t *history )
+{
+    bool released = false;
+    if ( ( *history & DEBOUNCE_MASK ) == 0b11000000 ) {
+        released = true;
+        *history = 0b00000000;
+    }
+    return released;
 }
 
-uint8_t debounce_is_button_up(uint8_t *history){
-  return *history == 0x00;
+bool debounce_is_button_down ( uint8_t *history )
+{
+    return *history == 0b11111111;
+}
+
+bool debounce_is_button_up ( uint8_t *history )
+{
+    return *history == 0b00000000;
 }
--- debounce/debounce.h
+++ debounce/debounce.h
@@ -1,23 +1,38 @@
-/*
- * De-bouncing functions accoding to
- * http://hackaday.com/2015/12/10/embed-with-elliot-debounce-your-noisy-buttons-part-ii/
+/**
+ * @file debounce.h
+ * @author Stefan Haun (tux@netz39.de)
  *
- * Author: tux@netz39.de
+ * @brief Pattern-based de-bouncing functions
  *
  * Note: The button is evaluated with a positive logic, i.e.
  *       generates a logical 1 if the button is hold down and a
  *       logical 0 if the button is up.
  *       The history, however, is stored inverted.
+ *
+ * @see http://hackaday.com/2015/12/10/embed-with-elliot-debounce-your-noisy-buttons-part-ii/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 /**
  * \brief Initialize the button to "button up".
  */
-void debounce_init_button(uint8_t *history);
+void debounce_init_button ( uint8_t *history );
 
 /**
  * \brief Update the button history.
@@ -28,8 +43,8 @@ void debounce_init_button(uint8_t *history);
  * Note: Remember to provide the input macro for the port,
  *       i.e. PINA instead of PORTA!
  */
-void debounce_update_button(uint8_t *history,
-			    uint8_t pinport, uint8_t pinpin);
+void debounce_update_button ( uint8_t *history,
+                              uint8_t pinport, uint8_t pinpin );
 
 /**
  * \brief Tell if the button has just been pressed.
@@ -40,7 +55,7 @@ void debounce_update_button(uint8_t *history,
  * to evaluate the pattern between subsequent history updates, otherwise
  * you may miss button press events!
  */
-uint8_t debounce_is_button_pressed(uint8_t *history);
+bool debounce_is_button_pressed ( uint8_t *history );
 
 /**
  * \brief Tell if the button has just been released.
@@ -51,18 +66,18 @@ uint8_t debounce_is_button_pressed(uint8_t *history);
  * to evaluate the pattern between subsequent history updates, otherwise
  * you may miss button release events!
  */
-uint8_t debounce_is_button_released(uint8_t *history);
+bool debounce_is_button_released ( uint8_t *history );
 
 /**
  * \brief Tell if the button is hold down.
  * \param history Pointer to an 8-bit button history.
  * \return 1 if the button is hold down, otherwise 0
  */
-uint8_t debounce_is_button_down(uint8_t *history);
+bool debounce_is_button_down ( uint8_t *history );
 
 /**
  * \brief Tell if the button is up.
  * \param history Pointer to an 8-bit button history.
  * \return 1 if the button is up, otherwise 0
  */
-uint8_t debounce_is_button_up(uint8_t *history);
+bool debounce_is_button_up ( uint8_t *history );
